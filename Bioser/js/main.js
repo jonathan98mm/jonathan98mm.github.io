9c2681c1f5e9f0a92216a0f618bce2f60a0be165
jonathan98mm <45021990+jonathan98mm@users.noreply.github.com>
2023-04-24 13:20:30 -0600

    Ahi va para que deje de chingar la diseñadora

---
+++ Bioser/js/main.js
@@ -0,0 +1,46 @@
+(function ($) {
+    "use strict";
+
+    AOS.init({
+        duration: 800,
+        easing: 'slide',
+        once: true
+    });
+
+    var rellax = new Rellax('.rellax');
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
