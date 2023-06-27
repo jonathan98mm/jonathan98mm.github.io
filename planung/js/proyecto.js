47209564439a369ded6494785cfa50c09bf6a0c0
jonathan98mm <45021990+jonathan98mm@users.noreply.github.com>
2023-06-27 17:57:07 -0600

    Para mañana el HO

---
+++ planung/js/proyecto.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.getElementsByTagName("nav")[0];
+
+    document.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            navbar.classList.add("Navbar__Active");
+        } else {
+            navbar.classList.remove("Navbar__Active");
+        }
+    });
+});
